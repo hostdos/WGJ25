e36e41b7c020951bf892b991f6f9930e1fa02ac0
Fizzzo <Fizzzo42@gmail.com>
2025-12-23 11:47:09 +0100

    improve idle upgrade ui with export variable

--- toy-box-25/scenes/ui/idle_upgrade.gd
+++ toy-box-25/scenes/ui/idle_upgrade.gd
@@ -1,6 +1,9 @@
 extends HBoxContainer
 
-var chickens_needed: int = 10
+@export var sprite_upgrade: Texture
+@export var sprite_spending_currency: Texture
+@export var base_price: int = 10
+@export var base_interval: int = 5
 
 var idle_upgrade_level: int = 0
 
@@ -10,24 +13,28 @@ func _ready():
 	%UpgradeButton.pressed.connect(_on_upgrade_button_clicked)
 	%Timer.timeout.connect(_on_timer_timeout)
 	_update_chickens_needed()
+	%UpgradeSprite.texture = sprite_upgrade
+	%SpendingCurrencySprite.texture = sprite_spending_currency
 	
-func _process(delta):
-	if %Timer.time_left > 0:
-		%ProgressBar.value = 1.0 - (%Timer.time_left / %Timer.wait_time)
+func _process(_delta):
+	if base_interval > 0:
+		if %Timer.time_left > 0:
+			%ProgressBar.value = 1.0 - (%Timer.time_left / %Timer.wait_time)
 	
 func _update_ui(amount_chickens: int):
-	%UpgradeButton.disabled = amount_chickens < chickens_needed
+	%UpgradeButton.disabled = amount_chickens < _get_price()
 
 func _on_upgrade_button_clicked():
-	if GameManager.amount_chickens >= chickens_needed:
-		GameManager.amount_chickens = GameManager.amount_chickens - chickens_needed
+	if GameManager.amount_chickens >= _get_price():
+		GameManager.amount_chickens = GameManager.amount_chickens - _get_price()
 		idle_upgrade_level = idle_upgrade_level + 1
 		_on_idle_upgrade_level_changed()
 
 func _on_idle_upgrade_level_changed():
-	%Timer.wait_time = 5
-	if %Timer.is_stopped():
-		%Timer.start()
+	if base_interval > 0:
+		%Timer.wait_time = base_interval
+		if %Timer.is_stopped():
+			%Timer.start()
 	_update_chickens_needed()
 	%LevelLabel.text = str(idle_upgrade_level)
 
@@ -35,5 +42,7 @@ func _on_timer_timeout():
 	GameManager.amount_chickens = GameManager.amount_chickens + idle_upgrade_level
 
 func _update_chickens_needed():
-	chickens_needed = 10 + idle_upgrade_level * 5
-	%UpgradeButton.text = str(chickens_needed) + "     "
+	%UpgradeButton.text = str(_get_price()) + "     "
+
+func _get_price():
+	return base_price + idle_upgrade_level * (base_price / 2)
