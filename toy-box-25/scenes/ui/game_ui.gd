79f16882608ebe33331d38c91a10f0672c9afd31
Fizzzo <Fizzzo42@gmail.com>
2025-12-23 19:59:09 +0100

    removed unnecesary buttons

--- toy-box-25/scenes/ui/game_ui.gd
+++ toy-box-25/scenes/ui/game_ui.gd
@@ -7,12 +7,11 @@ const color_unselected: Color = Color(0.13, 0.13, 0.13, 1.0)
 func _ready():
 	GameManager.amount_poulet_changed.connect(_update_poulet_amount)
 	GameManager.amount_chickens_changed.connect(_update_chicken_amount)
-	%TimerIdleUpgrade.on_progress_bar_full.connect(_add_chickens.bind(%TimerIdleUpgrade.idle_upgrade_level))
+	%TimerIdleUpgrade.on_progress_bar_full.connect(_add_chickens)
 	%MouseIdleUpgrade.on_upgraded.connect(_add_mouse)
 	%RobotIdleUpgrade.on_progress_bar_full.connect(_send_robot)
-	%AddChickenButton.pressed.connect(_add_chickens.bind(1))
-	_init_upgrade_size_buttons()
-	
+	%AddChickenButton.pressed.connect(_add_chicken)
+
 func _update_chicken_amount(_new_chickens: int):
 	%AmountChickens.text = str(GameManager.amount_chickens)
 
@@ -21,41 +20,12 @@ func _update_poulet_amount(_new_poulets: int):
 
 func _send_robot():
 	pass
+	
+func _add_chicken():
+	GameManager.amount_chickens += 1
 
-func _add_chickens(amount: int):
-	GameManager.amount_chickens += amount
+func _add_chickens():
+	GameManager.amount_chickens += %TimerIdleUpgrade.idle_upgrade_level
 
 func _add_mouse():
 	GameManager.amount_mice += 1
-
-func _init_upgrade_size_buttons():
-	%Upgrade1Button.pressed.connect(_on_upgrade_size_button_pressed.bind(1))
-	%Upgrade10Button.pressed.connect(_on_upgrade_size_button_pressed.bind(10))
-	%Upgrade25Button.pressed.connect(_on_upgrade_size_button_pressed.bind(25))
-	%UpgradeMaxButton.pressed.connect(_on_upgrade_size_button_pressed.bind(99))
-	_update_upgrade_size_buttons()
-	
-func _on_upgrade_size_button_pressed(upgrade_size: int):
-	self.upgrade_size = upgrade_size
-	_update_upgrade_size_buttons()
-
-func _update_upgrade_size_buttons():
-	_set_button_background_color(%Upgrade1Button, color_unselected)
-	_set_button_background_color(%Upgrade10Button, color_unselected)
-	_set_button_background_color(%Upgrade25Button, color_unselected)
-	_set_button_background_color(%UpgradeMaxButton, color_unselected)
-	match upgrade_size:
-		1:
-			_set_button_background_color(%Upgrade1Button, color_selected)
-		10:
-			_set_button_background_color(%Upgrade10Button, color_selected)
-		25:
-			_set_button_background_color(%Upgrade25Button, color_selected)
-		99:
-			_set_button_background_color(%UpgradeMaxButton, color_selected)
-
-func _set_button_background_color(button: Button, color: Color):
-	for state in ["normal", "hover", "pressed", "disabled"]:
-		var style := StyleBoxFlat.new()
-		style.bg_color = color
-		button.add_theme_stylebox_override(state, style)
