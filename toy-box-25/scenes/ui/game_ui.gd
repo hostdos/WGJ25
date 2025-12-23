3cd736ee987a1e1e20e447b69619e92bf77a8242
Fizzzo <Fizzzo42@gmail.com>
2025-12-23 15:56:35 +0100

    added chickens back to UI. WE BACK

--- toy-box-25/scenes/ui/game_ui.gd
+++ toy-box-25/scenes/ui/game_ui.gd
@@ -6,13 +6,17 @@ const color_unselected: Color = Color(0.13, 0.13, 0.13, 1.0)
 
 func _ready():
 	GameManager.amount_poulet_changed.connect(_update_poulet_amount)
+	GameManager.amount_chickens_changed.connect(_update_chicken_amount)
 	%TimerIdleUpgrade.on_progress_bar_full.connect(_add_chicken)
 	%MouseIdleUpgrade.on_upgraded.connect(_add_mouse)
 	%AddChickenButton.pressed.connect(_add_chicken)
 	_init_upgrade_size_buttons()
 
-func _update_poulet_amount(_new_chickens: int):
-	%AmountChickens.text = str(GameManager.amount_poulet)
+func _update_chicken_amount(_new_chickens: int):
+	%AmountChickens.text = str(GameManager.amount_chickens)
+
+func _update_poulet_amount(_new_poulets: int):
+	%AmountPoulets.text = str(GameManager.amount_poulet)
 
 func _add_chicken():
 	GameManager.amount_chickens += 1
