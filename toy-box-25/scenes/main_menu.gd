22b8f108b236a242339ce8096af713af577cc990
Dominique Hostettler <dominiquehostettler@gmail.com>
2025-12-24 03:53:46 +0100

    damn we got a new main scene

--- toy-box-25/scenes/main_menu.gd
+++ toy-box-25/scenes/main_menu.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @export var discordlink: String = "https://discord.gg/Fn7VE7xvZ3"
+@export var animator: AnimationPlayer
 
 func _ready():
 	%StartButton.pressed.connect(_on_start_button_pressed)
@@ -9,7 +10,7 @@ func _ready():
 
 func _on_start_button_pressed():
 	print("Start Button pressed")
-	%MainMenuAnimation.play("Intro")
+	animator.play("Intro")
 
 func _on_exit_button_pressed():
 	print("Quit Button pressed")
@@ -19,4 +20,5 @@ func _on_discord_button_pressed():
 	OS.shell_open(discordlink)
 
 func go_to_game():
-	get_tree().change_scene_to_file("res://scenes/map.tscn")
+	#get_tree().change_scene_to_file("res://scenes/map.tscn")
+	pass
